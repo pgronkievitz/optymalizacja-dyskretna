6f4ea26e197dce165732a68739987552160d579b
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-05-30 10:54:03 +0200

    fix index
    All asserts passed
    
    Signed-off-by: Piotr Krawiec <piotr.krawiec23@gmail.com>

--- alg.py
+++ alg.py
@@ -20,10 +20,6 @@ for i in range(1, t.shape[0]):
         tk[i,j] = tp[i,j] + t[i,j]
 
 
-print(t)
-print(tp)
-print(tk)
-
 ######### Permutacje
 perm = np.array([2,3])
 
@@ -50,17 +46,46 @@ def get_permutation(arr: np.array, permutaion : np.array):
     p = np.array([x for x in range(0, arr.shape[1])])
     return arr[:,np.concatenate((permutaion, np.setdiff1d(p, permutaion)))]
 
+def calc(t, r, phi_index):
+    t = get_permutation(t, r)
 
-r = [0] # Permutation
-phi_index = 0 # Phi index
-t = get_permutation(t, r)
-p = np.array([x for x in range(0, t.shape[1])]) # Columns index
-k = np.array([x for x in range(0, t.shape[0])]) # Rows index
-cols = np.setdiff1d(p, r) # Excluded columns
-print(cols)
-print(t)
-print(tk[phi_index, len(r)-1])
-print(np.sum(t[phi_index, cols]))
-print(np.min(np.sum(t[:,len(r):][np.setdiff1d(k, phi_index),:],axis=0)))
-print(t[:,len(r):][np.setdiff1d(k, phi_index),:])
+    tp, tk = macierze_czasow(t)
+
+    p = np.array([x for x in range(0, t.shape[1])]) # Columns index
+    k = np.array([x for x in range(0, t.shape[0])]) # Rows index
+    cols = np.setdiff1d(p, r) # Excluded columns
+    if phi_index == 0:
+        suma = np.sum(t[phi_index,])
+    else:
+        suma = np.sum(t[phi_index,len(r):]) + tk[phi_index, len(r)-1]
+    minimum = np.min(np.sum(t[phi_index+1:,:][:,len(r):],axis=0))
+    return suma + minimum
+
+
+def macierze_czasow(t):
+    tp = np.zeros(t.shape)
+    tk = np.zeros(t.shape)
+    tp[0] = np.cumsum(np.concatenate((np.array([0]), t[0][:-1])))
+    tp[:, 0] = np.cumsum(np.concatenate((np.array([0]), t[:, 0][:-1])))
+    tk[0] = np.cumsum(t[0])
+    tk[:, 0] = np.cumsum(t[:, 0])
+    for i in range(1, t.shape[0]):
+        for j in range(1, t.shape[1]):
+            tp[i, j] = max([tk[i - 1, j], tk[i, j - 1]])
+            tk[i, j] = tp[i, j] + t[i, j]
+
+    return tp, tk
+
+
+assert (calc(t,[0], 0)) == 31
+assert (calc(t,[1], 0)) == 32
+assert (calc(t,[2], 0)) == 31
+
+assert (calc(t,[0], 1)) == 30
+assert (calc(t,[1], 1)) == 31
+assert (calc(t,[2], 1)) == 29
+
+assert (calc(t,[0,1], 0)) == 34
+assert (calc(t,[0,2], 0)) == 31
+assert (calc(t,[0,3], 0)) == 31
 
