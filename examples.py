9a32679775f4e81d6a60b9f954446196e8577de4
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-06-05 12:13:53 +0200

    reformat
    Signed-off-by: Piotr Krawiec <piotr.krawiec23@gmail.com>

--- examples.py
+++ examples.py
@@ -1,19 +1,16 @@
 import numpy as np
 from lomnicki import lomnicki
 
-zadanie1 = np.array([
+# Zadanie przykładowe
+dane = np.array([
    [4, 8, 1],
    [2, 3, 7],
    [6, 2, 5]
 ])
-print(lomnicki(zadanie1))
+print(dane)
+print(lomnicki(dane))
 
-zadanie2 = np.array([
-   [6, 4, 5, 7],
-   [3, 6, 4, 8],
-   [7, 3, 8, 6]
-])
-print(lomnicki(zadanie2))
-
-zadanie3 = np.random.randint(1,15, size=(10,5))
-print(lomnicki(zadanie3))
+# Zadanie duże
+dane = np.random.randint(1, 15, size=(10, 5))
+print(dane)
+print(lomnicki(dane))
